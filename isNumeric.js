4e59eb6714e07b15467c711dbbcb0f50d58d525c
RC-24 <cullotyross@eircom.net>
2018-04-02 20:11:49 +0100

    Added script to check if user input is numeric

---
+++ isNumeric.js
@@ -0,0 +1,7 @@
+var input = Number(prompt("Enter a number"));
+if (Number.isNaN(input)) {
+    alert("The value you entered is not numeric.");
+}
+else {
+    alert("You entered the numeric value: " + input);
+}
---
+++ isNumeric.ts
@@ -0,0 +1,8 @@
+let input: number = Number(prompt("Enter a number"));
+
+if (Number.isNaN(input)) {
+    alert("The value you entered is not numeric.");
+}
+else {
+    alert("You entered the numeric value: " + input);
+}
