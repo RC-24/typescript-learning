8b624632a9000c99e6544b148ddbcc492945caaa
RC-24 <cullotyross@eircom.net>
2018-04-02 15:32:21 +0100

    Created script to add two numbers and print the result to the console

---
+++ sumOfTwoNumbers.js
@@ -0,0 +1,4 @@
+function getSum(number1, number2) {
+    return number1 + number2;
+}
+console.log(getSum(22, 19));
---
+++ sumOfTwoNumbers.ts
@@ -0,0 +1,5 @@
+function getSum(number1: number, number2: number) : number{
+    return number1 + number2;
+}
+
+console.log(getSum(22, 19));
